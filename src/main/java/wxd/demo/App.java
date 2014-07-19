0c7679ad8fadfb7a9ce678e1e91e50247d6942ee
wgitage <wgitage@163.com>
2014-07-19 22:22:37 +0800

    ad

---
+++ src/main/java/wxd/demo/App.java
@@ -0,0 +1,14 @@
+package wxd.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+    }
+}
